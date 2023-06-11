6ca863078f3947c7ea7c4a6f3c231caf52e81474
Alesanzz <alejandro240295@hotmail.com>
2023-06-11 19:58:03 -0300

    Improving the project's modularization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 import express from "express";
-//requiriendo el npm de socket para poder usar este tipo de servidores
-import { Server } from "socket.io";
 
 import { sourceDirname } from "./middlewares/dirname.js";
 import { routerVistaChat } from "./routes/chat.vista.router.js";
@@ -29,52 +27,10 @@ app.use("/chat-socket", routerVistaChat);
 app.use("/realtimeproducts", routerRealTimeProducts);
 
 
-//para confirgurar el servidor socket hay que guardar el servidor http en una variable y luego ejecurtar el "Server" de socket.io sobre nuestro servidor http
+//para confirgurar el servidor socket hay que importar el archivo donde se encuentra toda la logica del socket server, luego guardar el servidor http en una variable y por ultimo ejecurtar el "servidor" de socket.io sobre nuestro servidor http
+import { connectSocket } from "./socket-server.js";
 const httpServer = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-const socketServer = new Server(httpServer);
 
-
-//socket para el chat comunitario
-let msgs = [];
-socketServer.on("connection", (socket) => {
-  socket.on("msg_front_to_back", (msg) => {
-    msgs.push(msg);
-    console.log(msgs);
-    //socket.on = es para configurar que se debe estar pendiente de escuchar cuando envien el primer parametro del front
-    //primer parametro = nombre del parametro a escuchar
-    //segundo parametro = un objeto que se recibe del front
-    socketServer.emit("msg_back_to_front", msgs);
-    //socketServer.emit = es para realizar un envio de informacion del back al front a todos los canales/usuarios con el front (recordemos que existe un canal socket de front por cada chat/usuario que acceda a nuestra pagina)
-    //primer parametro = nombre del parametro a enviar
-    //segundo parametro = un objeto que se manda al front
-  });
-});
-
-//socket para el product manager
-import { productManager } from "./modules/products-manager.js";
-socketServer.on("connection", (socket) => {
-  socket.on("new-product-created", async (newProduct) => {
-    try {
-      await productManager.addProduct(newProduct);
-      
-      let allProducts = await productManager.getProducts();
-      socketServer.emit("all-the-products", allProducts);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-  
-  socket.on("delete-product", async (iidd) => {
-    try {
-      await productManager.deleteProduct(iidd);
-  
-      let allProducts = await productManager.getProducts();
-      socketServer.emit("all-the-products", allProducts);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-}); 
-
+connectSocket(httpServer);
